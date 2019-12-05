3a20b185f4938fde3fbd5b0793827b7625706a78
kushanPC <37808932+kushanPC@users.noreply.github.com>
2019-12-05 22:37:57 +0200

    editing

--- src/js/main.js
+++ src/js/main.js
@@ -2,57 +2,53 @@
 import dataSlides from './data.js';
 
 let currentImg = 0;
-let slide = document.getElementById('image');
-
 let div;
-let pagination = document.getElementById('pagination');
-let prevSlide =  document.getElementsByClassName('btn-left');
-let nextSlide =  document.getElementsByClassName('btn-right');
 
-let dotArray =  document.getElementsByClassName("dot");
+const slide = document.getElementById('image');
+const pagination = document.getElementById('pagination');
+const prevSlide =  document.getElementsByClassName('btn-left');
+const nextSlide =  document.getElementsByClassName('btn-right');
+const dotArray =  document.getElementsByClassName("dot");
 
 function slider(image) {
- 	slide.style.backgroundImage = 'url('+dataSlides.slides[image]+')';
+  slide.style.backgroundImage = `url(${dataSlides.slides[image]})`;
 
   for (let image = 0; image < dotArray.length; image++) {
     dotArray[image].className = 'dot';
   }
 
-  dotArray[image].className = 'dot dot-active';
+  currentImg = image;
+
+  dotArray[image].className = 'dot active';
 }
 
-prevSlide[0].onclick = function() {
+prevSlide[0].addEventListener("click", function(){
+	currentImg-=1;
   if (currentImg < 0) {
-    currentImg =  dataSlides.slides.length-1;
+    currentImg = dataSlides.slides.length - 1;
   }
-	currentImg--;
 	slider(currentImg);
-}
+},false);
 
-nextSlide[0].onclick = function() {
-  if (currentImg == dataSlides.slides.length) {
+nextSlide[0].addEventListener("click", function(){
+	currentImg+=1;
+  if (currentImg === dataSlides.slides.length) {
     currentImg = 0;
   }
-	currentImg++;
 	slider(currentImg);
-}
+},false);
 
-for (let i = 0; i < dataSlides.slides.length; i++) {
-  div =  document.createElement('div');
+dataSlides.slides.forEach(function() {
+  div = document.createElement('div');
   div.className = "dot";
   pagination.append(div);
-}
+});
 
 for (let i = 0; i < dotArray.length; i++) {
   dotArray[i].onclick = function() {
     let num = i;
     slider(num);
-
-    for (let j = 0; j < dotArray.length; j++) {
-      dotArray[j].className = 'dot';
-    }
-
-    this.className = 'dot dot-active';
+    this.className = 'dot active';
   };
 }
 
