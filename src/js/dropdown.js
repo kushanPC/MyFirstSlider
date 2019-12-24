0b9c7906e042c5958934c752425320e8663b1e8c
kushanPC <37808932+kushanPC@users.noreply.github.com>
2019-12-24 17:33:08 +0200

    add husky, eslint airbnb and stylelint

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const buttonDropdown = document.querySelector('.btn-dropdown');
 const dropdownList = document.querySelector('.dropdown');
 const dropItems = document.getElementsByClassName('item');
@@ -8,11 +6,11 @@ const itemsToFind = document.querySelectorAll('.country-name');
 
 function generateButtonText() {
   let buttonText = '';
-  let selectedItems = [];
+  const selectedItems = [];
 
-  for (let i = 0; i < checkBox.length; i++) {
-    if (checkBox[i].checked){
-      selectedItems.push(itemsToFind[i].innerText + ' ');
+  for (let i = 0; i < checkBox.length; i += 1) {
+    if (checkBox[i].checked) {
+      selectedItems.push(`${itemsToFind[i].innerText} `);
       checkBox[i].parentNode.classList.add('selected');
     } else {
       checkBox[i].parentNode.classList.remove('selected');
@@ -20,56 +18,56 @@ function generateButtonText() {
   }
 
   if (selectedItems.length > 3) {
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 3; i += 1) {
       buttonText += selectedItems[i];
     }
     buttonText += '...';
   } else {
-    for (let i = 0; i < selectedItems.length; i++) {
+    for (let i = 0; i < selectedItems.length; i += 1) {
       buttonText += selectedItems[i];
     }
   }
 
   if (Boolean(buttonText) === false) {
-    buttonText = 'Dropdown'
+    buttonText = 'Dropdown';
   }
   return buttonText;
 }
 
-function clickToItem(liTag) {
+function clickToItem(liTage) {
+  const liTag = liTage;
   if (liTag.className === 'item') {
-    liTag.className = 'item selected';
+    liTag.classList = 'item selected';
     liTag.childNodes[3].checked = true;
     buttonDropdown.textContent = generateButtonText();
   } else {
-    liTag.className = 'item';
+    liTag.classList = 'item';
     liTag.childNodes[3].checked = false;
     buttonDropdown.textContent = generateButtonText();
   }
 }
 
-function clickToCheckAll(liTag) {
+function clickToCheckAll(liTage) {
+  const liTag = liTage;
   if (liTag.className === 'checked') {
-    liTag.className = 'checked selected';
+    liTag.classList = 'checked selected';
     liTag.childNodes[3].checked = true;
 
-    for (var i = 0; i < checkBox.length; i++) {
-      if (checkBox[i].parentNode.className == 'item hide') {
-        continue;
+    for (let i = 0; i < checkBox.length; i += 1) {
+      if (checkBox[i].parentNode.className !== 'item hide') {
+        checkBox[i].checked = true;
       }
-      checkBox[i].checked = true;
     }
 
     buttonDropdown.textContent = generateButtonText();
   } else if (liTag.className === 'checked selected') {
-    liTag.className = 'checked';
+    liTag.classList = 'checked';
     liTag.childNodes[3].checked = false;
 
-    for (var i = 0; i < checkBox.length; i++) {
-      if (checkBox[i].parentNode.className === 'item hide') {
-        continue;
+    for (let i = 0; i < checkBox.length; i += 1) {
+      if (checkBox[i].parentNode.className !== 'item hide') {
+        checkBox[i].checked = false;
       }
-      checkBox[i].checked = false;
     }
 
     buttonDropdown.textContent = generateButtonText();
@@ -77,15 +75,14 @@ function clickToCheckAll(liTag) {
 }
 
 function clickToList() {
-  let item = event.target;
-
-  if (item.dataset.target != 'target') {
+  const item = event.target;
+  if (item.dataset.target !== 'target') {
     return;
-  } else if (item.className === 'item' || item.className === 'item selected') {
+  } if (item.className === 'item' || item.className === 'item selected') {
     clickToItem(item);
   } else if (item.className === 'country-name' || item.className === 'checkbox') {
     clickToItem(item.parentNode);
-  }  else if (item.className === 'checked' || item.className === 'checked selected') {
+  } else if (item.className === 'checked' || item.className === 'checked selected') {
     clickToCheckAll(item);
   } else if (item.className === 'checked-all' || item.className === 'checkbox-select-all') {
     clickToCheckAll(item.parentNode);
@@ -95,44 +92,44 @@ function clickToList() {
 
 function clickToDropdownButton() {
   if (dropdownList.className === 'dropdown') {
-    dropdownList.className = 'dropdown show';
+    dropdownList.classList = 'dropdown show';
 
-    for (var i = 0; i < dropItems.length; i++) {
-      dropItems[i].className = 'item';
+    for (let i = 0; i < dropItems.length; i += 1) {
+      dropItems[i].classList = 'item';
     }
   } else {
-    dropdownList.className = 'dropdown';
-    buttonDropdown.textContent = `Dropdown`;
+    dropdownList.classList = 'dropdown';
+    buttonDropdown.textContent = 'Dropdown';
   }
 }
 
 function markText(string, pos, len) {
-  return string.slice(0, pos) + '<mark>' + string.slice(pos, pos + len) + '</mark>' + string.slice(pos + len);
+  return `${string.slice(0, pos)}<mark>${string.slice(pos, pos + len)}</mark>${string.slice(pos + len)}`;
 }
-
 buttonDropdown.addEventListener('click', clickToDropdownButton);
-
 dropdownList.addEventListener('click', clickToList);
+document.querySelector('#text-to-find').addEventListener('input', function findItem() {
+  const expression = this.value.trim();
+  const val = new RegExp(expression, 'i');
 
-document.querySelector('#text-to-find').oninput = function () {
-  let expression = this.value.trim();
-  let val = new RegExp(expression, 'i');
 
-  if (expression != '') {
-    itemsToFind.forEach(function (elem) {
+  if (expression !== '') {
+    itemsToFind.forEach((element) => {
+      const elem = element;
       if (elem.innerText.search(val) === -1) {
         elem.parentNode.classList.add('hide');
         elem.innerHTML = elem.innerText;
       } else {
         elem.parentNode.classList.remove('hide');
-        let str = elem.innerText;
+        const str = elem.innerText;
         elem.innerHTML = markText(str, elem.innerText.search(val), expression.length);
       }
     });
-  }  else {
-    itemsToFind.forEach(function (elem) {
+  } else {
+    itemsToFind.forEach((element) => {
+      const elem = element;
       elem.parentNode.classList.remove('hide');
       elem.innerHTML = elem.innerText;
     });
   }
-}
+});
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,4 +1,3 @@
-"use strict";
 import dataSlides from './data.js';
 
 let currentSlide = 0;
@@ -6,42 +5,38 @@ let div;
 
 const slide = document.getElementById('image');
 const pagination = document.getElementById('pagination');
-const prevSlide =  document.querySelector('.btn-left');
-const nextSlide =  document.querySelector('.btn-right');
-const dotArray =  document.getElementsByClassName('dot');
+const prevSlide = document.querySelector('.btn-left');
+const nextSlide = document.querySelector('.btn-right');
+const dotArray = document.getElementsByClassName('dot');
 
 let timerId =
-  setTimeout(function() {
+  setTimeout(() => {
     onClickNextSlide();
-  },8000);
+  }, 8000);
+
 
 function slider(image) {
   slide.style.backgroundImage = `url(${dataSlides.slides[image]})`;
 
-  for (let i = 0; i < dotArray.length; i++) {
-    dotArray[i].className = 'dot';
+  for (let i = 0; i < dotArray.length; i += 1) {
+    dotArray[i].classList = 'dot';
   }
 
   currentSlide = image;
 
-  dotArray[image].className = 'dot active';
+  dotArray[image].classList = 'dot active';
 }
 
-function onClickNextSlide(){
- currentSlide += 1;
- if (currentSlide === dataSlides.slides.length) {
-   currentSlide = 0;
- }
- slider(currentSlide);
+function onClickNextSlide() {
+  currentSlide += 1;
+  if (currentSlide === dataSlides.slides.length) {
+    currentSlide = 0;
+  }
+  slider(currentSlide);
 
-
- clearTimeout(timerId);
- timerId =
-   setTimeout(function() {
-     onClickNextSlide();
-   },8000);
+  clearTimeout(timerId);
+  timerId = setTimeout(onClickNextSlide, 8000);
 }
-
 function onClickPreviousSlide() {
   currentSlide -= 1;
   if (currentSlide < 0) {
@@ -51,18 +46,18 @@ function onClickPreviousSlide() {
 }
 
 function onclickPagination() {
-  let dot = event.target;
+  const dot = event.target;
 
-  if (dot.className != 'dot') {
+  if (dot.className !== 'dot') {
     return;
   }
 
-  for (let i = 0; i < dotArray.length; i++) {
-    dotArray[i].className = 'dot';
+  for (let i = 0; i < dotArray.length; i += 1) {
+    dotArray[i].classList = 'dot';
   }
 
   slider(Number(dot.dataset.indexNumber));
-  dot.className = 'dot active';
+  dot.classList = 'dot active';
 }
 
 prevSlide.addEventListener('click', onClickPreviousSlide);
@@ -71,13 +66,13 @@ nextSlide.addEventListener('click', onClickNextSlide);
 
 pagination.addEventListener('click', onclickPagination);
 
-dataSlides.slides.forEach(function(item, i, arr) {
+dataSlides.slides.forEach((item, i) => {
   div = document.createElement('div');
   div.dataset.indexNumber = i;
-  div.className = 'dot';
+  div.classList = 'dot';
   pagination.append(div);
 });
 
-window.onload = function() {
-	slider(currentSlide);
+window.onload = function startSlider() {
+  slider(currentSlide);
 };
